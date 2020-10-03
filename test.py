6845eba71808e5fae751a4209f748fd4e7edd70b
Devon Edwards Joseph <devonedwardsjoseph@devons-mbp.home>
2020-10-03 16:15:12 +0100

    add draft of next test

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import pytest 
 
-from main import validate_args, filter_users_by_name
+from main import(
+    validate_args,
+    filter_users_by_name,
+    create_banned_foods_dict
+)
 
 
 @pytest.mark.parametrize(
@@ -27,7 +31,6 @@ def test_validate_args_fail(acceptable_args, actual_args):
     assert pytest_wrapped_e.type == SystemExit
 
 
-
 @pytest.mark.parametrize(
     "names, users, expected_output",
     [(["sarah", "max"], 
@@ -35,12 +38,62 @@ def test_validate_args_fail(acceptable_args, actual_args):
      {"max":{"wont_eat": ["crab"], "drinks": ["tea"]}, "sarah": {"wont_eat": ["pies"], "drinks": ["everything"]}})
     ])
 def test_filter_users_by_name_pass(names, users, expected_output):
-
     filtered_users = filter_users_by_name(names, users)
-
-    print(filtered_users)
-
     assert filtered_users == expected_output
 
+
+
+# @pytest.mark.parametrize(
+#     "desired_key, desired_value, args, all_users, filtered_users, expected_output",
+
+#     [(
+#     ["wont_eat"], 
+#     ["name"], 
+#     ["sarah", "max"], 
+
+#     [{"name":"sarah", "wont_eat": ["pies"], "drinks": ["everything"]},{"name":"david", "wont_eat": ["mango"], "drinks": ["cola"]}, { "name":"max", "wont_eat": ["crab"], "drinks": ["tea"]}],
+
+#     {"max":{"wont_eat": ["crab"], "drinks": ["tea"]}, "sarah": {"wont_eat": ["pies"], "drinks": ["everything"]}},
+
+#     {"crab": ["max"],
+#     "pies": ["sarah"]}  )]
+#     )
+# def test_create_banned_foods_dict(desired_key, desired_value, args, all_users, filtered_users, expected_output):
+
+#     banned_foods_dict = create_banned_foods_dict(desired_key, desired_value, args, all_users, filtered_users)
+
+#     assert banned_foods_dict == expected_output
+
+
+
+# @pytest.mark.parametrize(
+#     "desired_key, desired_value, args, all_users, filtered_users, expected_output",
+
+#     [(
+#     ["wont_eat"], 
+#     ["name"], 
+#     ["sarah", "max"], 
+
+#     [{"name":"sarah", "wont_eat": ["pies"], "drinks": ["everything"]},{"name":"david", "wont_eat": ["mango"], "drinks": ["cola"]}, { "name":"max", "wont_eat": ["crab"], "drinks": ["tea"]}],
+
+#     {"max":{"wont_eat": ["crab"], "drinks": ["tea"]}, "sarah": {"wont_eat": ["pies"], "drinks": ["everything"]}},
+
+#     {"crab": ["max"],
+#     "pies": ["sarah"]}  )]
+#     )
+# def test_create_banned_foods_dict(desired_key, desired_value, args, all_users, filtered_users, expected_output):
+
+#     banned_foods_dict = create_banned_foods_dict(desired_key, desired_value, args, all_users, filtered_users)
+
+#     assert banned_foods_dict == expected_output
+
+
+
+evaluate_venues_for_suitability
+
+
+
+
+create_response
 
 

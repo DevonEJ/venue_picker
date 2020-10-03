218acee928c52de737997664d871da7a39a5b754
Devon Edwards Joseph <devonedwardsjoseph@devons-mbp.home>
2020-10-03 13:18:28 +0100

    add main func and tidy into functions for testing

--- main.py
+++ main.py
@@ -3,148 +3,168 @@ import sys
 import getopt
 import copy
 from itertools import chain
+from typing import Dict, List, Any
 
 
-with open("data/users.json", "r") as inputs:
-    all_users = json.load(inputs)
-    inputs.close()
+#TODO - Docstrings
 
-with open("data/venues.json", "r") as inputs:
-    all_venues = json.load(inputs)
-    inputs.close()
-
-
-user_names = [user["name"] for user in all_users]
-
-args = sys.argv[1:]
-for arg in args:
-    if arg == "everyone":
-        print("You're taking the whole team!")
-        break
-    if arg not in user_names:
-        print(f"Sorry, this is not a valid user. You can choose from: {user_names}")
-        sys.exit(2)
-    print("Yep, accepted")
+def retrieve_json_from_file(file_path:str ) -> List[Dict]:
+    """
+    """
+    try:
+        with open(file_path, "r") as inputs:
+            data = json.load(inputs)
+            inputs.close()
+            return data
+    except IOError as e:
+        print(f"Unable to read file at path {file_path}: {e}")
 
 
-# Filter the users list by those users that are going for food preferences
-filtered_users = {user["name"]: { "wont_eat": user["wont_eat"], "drinks": user["drinks"]} for user in all_users if user["name"] in args}
+def validate_args(acceptable_args: List[str], actual_args: List[str]) -> List[str]:
+    """
+    """
+    for arg in actual_args:
+        arg = arg.strip()
+        if arg == "everyone":
+            print("You're taking the whole team!")
+            break
+        if arg not in acceptable_args:
+            print(f"Sorry, this is not a valid user. You can choose from: {acceptable_args}")
+            sys.exit(2)
+        print("Yep, accepted")
+        return actual_args
 
 
-# SORT OUT THE FOODS
-# Get what they won't eat
-banned_foods = [[food for food in user["wont_eat"]] for user in all_users if user["name"] in args] 
-banned_foods_flattened = list(chain.from_iterable(banned_foods))
+def filter_users_by_name(names: List[str], users: Dict[str, Any]) -> Dict[str, Any]:
+    """
+    """
+    # Remove users who not present in the specified args
+    filtered_users = {user["name"]: {"wont_eat": user["wont_eat"], "drinks": user["drinks"]} for user in users if user["name"] in names}
+    return filtered_users
 
-# Create dict to map banned foods to team members
-banned_foods_dict = {food: [] for food in banned_foods_flattened}
 
-for user, details in filtered_users.items():
-    for food in details["wont_eat"]:
-        if food in banned_foods_flattened:
-            banned_foods_dict[food].append(user)
+
+
+
+
+
+
+
+
+# # SORT OUT THE FOODS
+# # Get what they won't eat
+# banned_foods = [[food for food in user["wont_eat"]] for user in all_users if user["name"] in args] 
+# banned_foods_flattened = list(chain.from_iterable(banned_foods))
+
+# # Create dict to map banned foods to team members
+# banned_foods_dict = {food: [] for food in banned_foods_flattened}
+
+# for user, details in filtered_users.items():
+#     for food in details["wont_eat"]:
+#         if food in banned_foods_flattened:
+#             banned_foods_dict[food].append(user)
 
     
 
-# SORT OUT THE DRINKS
+# # SORT OUT THE DRINKS
+# # Get their favourite drinks
+# preferred_drinks = [[drink for drink in user["drinks"]] for user in all_users if user["name"] in args] 
+# preferred_drinks_flattened = list(chain.from_iterable(preferred_drinks))
 
-# Get their favourite drinks
-preferred_drinks = [[drink for drink in user["drinks"]] for user in all_users if user["name"] in args] 
-preferred_drinks_flattened = list(chain.from_iterable(preferred_drinks))
+# # Create dict to map drink preferences to team members
+# preferred_drinks_dict = {drink: [] for drink in preferred_drinks_flattened}
 
-# Create dict to map drink preferences to team members
-preferred_drinks_dict = {drink: [] for drink in preferred_drinks_flattened}
-
-for user, details in filtered_users.items():
-    for drink in details["drinks"]:
-        if drink in preferred_drinks_flattened:
-            preferred_drinks_dict[drink].append(user)
-
-
-
-venues_response = {"places_to_visit": [],
-                    "places_to_avoid":[]}
-
-avoid_venue_dict = {
-    "name": "",
-    "reason": []
-}
-
-venues_food_pass = [] 
-venues_food_fail = [] 
-
-venues_drink_pass = [] 
-venues_drink_fail = []
-# Filter venues by foods first
-for venue in all_venues:
-
-    venue_foods = set(venue["food"])
-
-    problem_foods = venue_foods.intersection(set(banned_foods_dict.keys()))
-
-    if len(problem_foods) > 0:
-        for food in problem_foods:
-            for user in banned_foods_dict[food]:
-                reason = f"There is nothing for {user} to eat."
-                result = copy.deepcopy(avoid_venue_dict)
-                result["name"] = venue["name"]
-                result["reason"].append(reason)
-                venues_food_fail.append(result)
-        else:
-            venues_food_pass.append(venue["name"])
-
-
-    # First eliminate drinks that none of the users want to drink
-    venue_drinks = [drink for drink in venue["drinks"] if drink in preferred_drinks_dict.keys()]
-
-    for drink in venue_drinks:
-        # If all users are ok with this drink, then this venue passes on drinks
-        if len(preferred_drinks_dict[drink]) == len(filtered_users.keys()):
-            # IF NOT ALRAEDY IN FOOD FAIL LIST
-            print(f"Current drink is preferred by all users")
-            venues_drink_pass.append(venue["name"])
-        else:
-            drinkless_users = filtered_users.keys() - preferred_drinks_dict[drink]
-            print(f"These are drinkless users: {drinkless_users}")
-            reason = f"There is nothing for {user} to drink."
-            # IF NOT ALRAEDY IN FOOD FAIL LIST
-            result = copy.deepcopy(avoid_venue_dict)
-            result["name"] = venue["name"]
-            result["reason"].append(reason)
-            venues_drink_fail.append(result)
-
-
-        
+# for user, details in filtered_users.items():
+#     for drink in details["drinks"]:
+#         if drink in preferred_drinks_flattened:
+#             preferred_drinks_dict[drink].append(user)
 
 
 
-    #     drinkless_users = banned_foods_dict.keys() - venue["drinks"]
-    #     print(drinkless_users)
+# venues_response = {"places_to_visit": [],
+#                     "places_to_avoid":[]}
 
-    #     for user in drinkless_users:
-            reason = f"There is nothing for {user} to drink."
-            # IF NOT ALRAEDY IN FOOD FAIL LIST
-            result = copy.deepcopy(avoid_venue_dict)
-            result["name"] = venue["name"]
-            result["reason"].append(reason)
-            venues_drink_fail.append(result)
+# avoid_venue_dict = {
+#     "name": "",
+#     "reason": []
+# }
+
+# venues_food_pass = [] 
+# venues_drink_pass = [] 
+# venues_failing = []
+# # Filter venues by foods first
+# for venue in all_venues:
+
+#     venue_result = copy.deepcopy(avoid_venue_dict)
+
+#     problem_foods = list(set(venue["food"]) & set(banned_foods_dict.keys()))
+
+#     if len(problem_foods) > 0:
+#         for food in problem_foods:
+#             for user in banned_foods_dict[food]:
+#                 reason = f"There is nothing for {user} to eat."
+#                 venue_result["name"] = venue["name"]
+#                 venue_result["reason"].append(reason)
+#         else:
+#             venues_food_pass.append(venue["name"])
 
 
-# print(venues_drink_fail)
-# print("Passing venues:")
-# print(set(venues_drink_pass))
+#     # First eliminate drinks that none of the users want to drink
+#     venue_drinks = [drink for drink in venue["drinks"] if drink in preferred_drinks_dict.keys()]
 
-            
-# print("Passing venues:")
-# print(set(venues_food_pass))
+#     for drink in venue_drinks:
+#         # If all users are ok with this drink, then this venue passes on drinks
+#         if len(preferred_drinks_dict[drink]) == len(filtered_users.keys()):
+#             # IF NOT ALRAEDY IN FOOD FAIL LIST
+#             venues_drink_pass.append(venue["name"])
+#         else:
+#             drinkless_users = filtered_users.keys() - preferred_drinks_dict[drink]
+#             reason = f"There is nothing for {user} to drink."
+#             # IF NOT ALRAEDY IN FOOD FAIL LIST
+#             result = copy.deepcopy(avoid_venue_dict)
+#             venue_result["name"] = venue["name"]
+#             venue_result["reason"].append(reason)
+
+#     venues_failing.append(venue_result)
+
+
+# #passing_venues = venues_drink_pass.intersection(venues_food_pass)
+# passing_venues = list(set(venues_drink_pass) & set(venues_food_pass))
+# print(passing_venues)
+
+
 # print("Failing venues:")
-# print(venues_food_fail)
+# print(venues_failing)
+# # print("Passing venues on drink:")
+# # print(set(venues_drink_pass))           
+# # print("Passing venues on food:")
+# # print(set(venues_food_pass))
+
     
+# #TODO - Warn user if a person entered is not in the users list
+# #TODO - add validation for the JSON data format, types etc.
+# #TODO - Add some tests (also for incorrect or no names)
+# # TODO - can you validate the input files against a schema?
+# # TODO - Are there any errors in the input files?
+# #TODO - add a help script
+# #TODO - Add main function
+
+if __name__ == "__main__":
+
+    all_users = retrieve_json_from_file("./data/users.json")
+
+    all_venues = retrieve_json_from_file("./data/venues.json")
+
+    user_names = [user["name"] for user in all_users]
+
+    args = sys.argv[1:]
+
+    args = validate_args(user_names, args)
+
+    filtered_users = filter_users_by_name(args, all_users)
+
+    print(filtered_users)
 
 
-#TODO - Warn user if a person entered is not in the users list
-#TODO - add validation for the JSON data format, types etc.
-#TODO - Add some tests (also for incorrect or no names)
-# TODO - can you validate the input files against a schema?
-# TODO - Are there any errors in the input files?
-#TODO - add a help script
+
+    print("Done.")

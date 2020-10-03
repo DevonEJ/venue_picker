454ad6b362e570268a4300bd702dbe639133be7e
Devon Edwards Joseph <devonedwardsjoseph@devons-mbp.home>
2020-10-03 16:00:44 +0100

    add test for transforming input

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import pytest 
 
-from main import validate_args
-
+from main import validate_args, filter_users_by_name
 
 
 @pytest.mark.parametrize(
@@ -26,5 +25,22 @@ def test_validate_args_fail(acceptable_args, actual_args):
     with pytest.raises(SystemExit) as pytest_wrapped_e:
             args = validate_args(acceptable_args, actual_args)
     assert pytest_wrapped_e.type == SystemExit
+
+
+
+@pytest.mark.parametrize(
+    "names, users, expected_output",
+    [(["sarah", "max"], 
+    [{"name":"sarah", "wont_eat": ["pies"], "drinks": ["everything"]},{"name":"david", "wont_eat": ["mango"], "drinks": ["cola"]}, { "name":"max", "wont_eat": ["crab"], "drinks": ["tea"]}],
+     {"max":{"wont_eat": ["crab"], "drinks": ["tea"]}, "sarah": {"wont_eat": ["pies"], "drinks": ["everything"]}})
+    ])
+def test_filter_users_by_name_pass(names, users, expected_output):
+
+    filtered_users = filter_users_by_name(names, users)
+
+    print(filtered_users)
+
+    assert filtered_users == expected_output
+
 
 
